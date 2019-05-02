6e48ee627609add8b183a457d72d11525f354475
aman.thakur <aman.thakur@accionlabs.com>
2019-05-02 18:49:58 +0530

    flutter_news app using expandableTile

---
+++ lib/src/article.dart
@@ -0,0 +1,110 @@
+class Article {
+  final String text;
+  final String domain;
+  final String by;
+  final String age;
+  final int score;
+  final int commentCount;
+
+  const Article({
+    this.text,
+    this.domain,
+    this.by,
+    this.age,
+    this.score,
+    this.commentCount,
+  });
+}
+
+final articles = [
+  new Article(
+    text:
+        "All attention has been focused on Bitcoin recently as it has led crypto markets off the bottom and back into bullish territory. Ethereum has been lack luster to say the least with very little movement in prices and certainly nothing to match other crypto asse… [+3274 chars]",
+    domain: "Newsbtc.com",
+    by: "Martin Young",
+    age: "2 hours",
+    score: 81,
+    commentCount: 126,
+  ),
+  new Article(
+    text:
+        "By CCN.com: Bitcoin played a role in staging the Easter Sunday suicide bombing in Sri Lanka that killed 253 civilians, according to Israeli blockchain intelligence firm Whitestream.\r\nThe agency revealed that the ISIS used Canada-based payment gateway CoinPaym… [+2542 chars]",
+    domain: "Crypto Coins News",
+    by: "Yashu Gola",
+    age: "2 hours",
+    score: 81,
+    commentCount: 126,
+  ),
+  new Article(
+    text:
+        "2019 has been an exciting year for Cardano, which made another significant step towards decentralisation with the Shelley Project. The recent Cardano news about the Shelley roadmap is a promise of a more robust blockchain run by community governance. \r\nShelle… [+5039 chars]",
+    domain: "Yahoo.com",
+    by: "Diana Aguilar",
+    age: "2 hours",
+    score: 81,
+    commentCount: 126,
+  ),
+  new Article(
+    text:
+        "Today marks a week since I left my home in Venezuela.\r\nSo, here I am, watching the news since 6 a.m., havent separated from my phone all day. Im worried about my loved ones, wondering if I could have done more before leaving, but knowing I had to leave anyway… [+5615 chars]",
+    domain: "Yahoo.com",
+    by: "Martin Young",
+    age: "2 hours",
+    score: 81,
+    commentCount: 126,
+  ),
+  new Article(
+    text:
+        "All attention has been focused on Bitcoin recently as it has led crypto markets off the bottom and back into bullish territory. Ethereum has been lack luster to say the least with very little movement in prices and certainly nothing to match other crypto asse… [+3274 chars]",
+    domain: "Newsbtc.com",
+    by: "Martin Young",
+    age: "2 hours",
+    score: 81,
+    commentCount: 126,
+  ),
+  new Article(
+    text:
+        "All attention has been focused on Bitcoin recently as it has led crypto markets off the bottom and back into bullish territory. Ethereum has been lack luster to say the least with very little movement in prices and certainly nothing to match other crypto asse… [+3274 chars]",
+    domain: "Newsbtc.com",
+    by: "Martin Young",
+    age: "2 hours",
+    score: 81,
+    commentCount: 126,
+  ),
+  new Article(
+    text:
+        "All attention has been focused on Bitcoin recently as it has led crypto markets off the bottom and back into bullish territory. Ethereum has been lack luster to say the least with very little movement in prices and certainly nothing to match other crypto asse… [+3274 chars]",
+    domain: "Newsbtc.com",
+    by: "Martin Young",
+    age: "2 hours",
+    score: 81,
+    commentCount: 126,
+  ),
+  new Article(
+    text:
+        "All attention has been focused on Bitcoin recently as it has led crypto markets off the bottom and back into bullish territory. Ethereum has been lack luster to say the least with very little movement in prices and certainly nothing to match other crypto asse… [+3274 chars]",
+    domain: "Newsbtc.com",
+    by: "Martin Young",
+    age: "2 hours",
+    score: 81,
+    commentCount: 126,
+  ),
+  new Article(
+    text:
+        "All attention has been focused on Bitcoin recently as it has led crypto markets off the bottom and back into bullish territory. Ethereum has been lack luster to say the least with very little movement in prices and certainly nothing to match other crypto asse… [+3274 chars]",
+    domain: "Newsbtc.com",
+    by: "Martin Young",
+    age: "2 hours",
+    score: 81,
+    commentCount: 126,
+  ),
+  new Article(
+    text:
+        "All attention has been focused on Bitcoin recently as it has led crypto markets off the bottom and back into bullish territory. Ethereum has been lack luster to say the least with very little movement in prices and certainly nothing to match other crypto asse… [+3274 chars]",
+    domain: "Newsbtc.com",
+    by: "Martin Young",
+    age: "2 hours",
+    score: 81,
+    commentCount: 126,
+  ),
+];
